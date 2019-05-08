26af10f0ef1dafaed19ab75cf8656b62d88cd067
Tanapat.Sir <>
2019-05-08 17:10:22 +0700

    no message

--- richmenu.php
+++ richmenu.php
@@ -1,7 +1,7 @@
 <?php 
 $channelAccessToken = "2tNAlMM4y3Rmj/BDGr7td82eUqUtj9dqQSobtPF/fDjGjm6G3ExSzbFX+GHbCoCYgb4l0Gg93j60hvPmi80bkXZJkypC9prbhEsJOBakNePZ6oaEj8rbbGeDfL+aW3SfgLOpnr8KVFhThk/pdY51XAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
  
-echo getListOfRichmenu($channelAccessToken);
+ print_r(getListOfRichmenu($channelAccessToken));
 
 function createNewRichmenu($channelAccessToken) {
   $sh = <<< EOF
@@ -27,7 +27,7 @@ function getListOfRichmenu($channelAccessToken) {
 EOF;
 
   $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
-  
+
   return $result;
 }
 
