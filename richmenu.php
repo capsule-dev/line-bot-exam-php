2bffa56b9c30c44dcb0c7184b7f552c8f39fad12
Tanapat.Sir <>
2019-05-08 17:32:38 +0700

    no message

--- richmenu.php
+++ richmenu.php
@@ -1,7 +1,6 @@
 <?php 
 $channelAccessToken = "2tNAlMM4y3Rmj/BDGr7td82eUqUtj9dqQSobtPF/fDjGjm6G3ExSzbFX+GHbCoCYgb4l0Gg93j60hvPmi80bkXZJkypC9prbhEsJOBakNePZ6oaEj8rbbGeDfL+aW3SfgLOpnr8KVFhThk/pdY51XAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
- 
- echo createNewRichmenu($channelAccessToken);
+ uploadImageRichmenu($channelAccessToken);
 
 function createNewRichmenu($channelAccessToken) {
   $sh = <<< EOF
@@ -94,20 +93,38 @@ function createNewRichmenu($channelAccessToken) {
 EOF;
   $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
   if(isset($result['richMenuId'])) {
-    $sheee = <<< EOF
-    curl -X POST \
-    -H 'Authorization: Bearer $channelAccessToken' \
-    -H 'Content-Type:image/jpeg' \
-    -T 'image.jpg' https://api.line.me/v2/bot/richmenu/{$result['richMenuId']}/content;
-    EOF;
-
-    $resule_sheee = shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sheee)))
-
-    return $resule_sheee;
+    return $result['richMenuId'];
   }
   else {
     return $result['message'];
   }
+}
+
+function uploadImageRichmenu($channelAccessToken){
+
+    $urlWithoutProtocol = "api.line.me/v2/bot/richmenu/{getListOfRichmenu($channelAccessToken)}/content";
+    $request         = "";
+    $isRequestHeader = false;
+ 
+    $exHeaderInfoArr   = array();
+    $exHeaderInfoArr[] = "Content-type: image/jpeg";
+    $exHeaderInfoArr[] = "Authorization: Bearer $channelAccessToken";
+ 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,array('file' => '@' . realpath('image.jpg') . ';filename=image.jpg'));
+    curl_setopt($ch, CURLOPT_HEADER, (($isRequestHeader) ? 1 : 0));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if( is_array($exHeaderInfo) && !empty($exHeaderInfo) )
+    {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $exHeaderInfo);
+    }
+    $response = curl_exec($ch);
+    curl_close($ch);
+ 
+    echo $response;
 }
 
 function getListOfRichmenu($channelAccessToken) {
