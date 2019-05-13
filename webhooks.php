b535ba99e5018635e4f90c92adc68b660c5790e2
Tanapat <tanapatsirisood@gmail.com>
2019-05-13 13:32:32 +0700

    k

--- webhooks.php
+++ webhooks.php
@@ -43,13 +43,16 @@
         $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][0]['text'] = "Body";
         $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][1]['type'] = "text";
         $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][1]['text'] = "ssssss";
+        $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][2]['type'] = "image";
+        $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][2]['url'] = "https://www.linefriends.com/img/img_sec.jpg";
+        $arrayPostData['messages'][0]['contents']['body']['contents'][0]['contents'][2]['size'] = "full";
 
 
-        $arrayPostData['messages'][0]['contents']['body']['contents'][1]['type'] = "box";
-        $arrayPostData['messages'][0]['contents']['body']['contents'][1]['layout'] = "vertical";
-        $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['type'] = "image";
-        $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['url'] = "https://www.linefriends.com/img/img_sec.jpg";
-        $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['size']= "full";
+        // $arrayPostData['messages'][0]['contents']['body']['contents'][1]['type'] = "box";
+        // $arrayPostData['messages'][0]['contents']['body']['contents'][1]['layout'] = "vertical";
+        // $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['type'] = "image";
+        // $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['url'] = "https://www.linefriends.com/img/img_sec.jpg";
+        // $arrayPostData['messages'][0]['contents']['body']['contents'][1]['contents']['size']= "full";
 
         $arrayPostData['messages'][0]['contents']['footer']['type'] = "box";
         $arrayPostData['messages'][0]['contents']['footer']['layout'] = "vertical";
